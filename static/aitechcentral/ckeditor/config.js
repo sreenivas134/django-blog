3dd4bf0dfe1a345c5c590fbac5be42f2296a0fa9
sreenivas134 <sreenivas.engece@gmail.com>
2019-02-03 17:51:04 +0530

    added text indent in editor tools

--- static/aitechcentral/ckeditor/config.js
+++ static/aitechcentral/ckeditor/config.js
@@ -8,6 +8,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
+	// config.language = 'en';
+	config.defaultLanguage = 'en';
+	config.extraPlugins = 'textindent';
+
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
@@ -29,7 +33,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	// config.removeButtons = 'Underline,Subscript,Superscript';
-
+	
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
