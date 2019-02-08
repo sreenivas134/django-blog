e71613b60b9f3b7616f85c31ec5ecf814c586a9a
sreenivas134 <sreenivas.engece@gmail.com>
2019-02-09 02:45:45 +0530

    removed /dashboard url from hit count url list

--- aitechcentral/centralMiddleware.py
+++ aitechcentral/centralMiddleware.py
@@ -15,7 +15,7 @@ def get_client_ip(request):
 def hit_count(request):
     if not request.session.session_key:
         request.session.save()
-    if request.path.startswith('/static') or request.path.startswith('/media'):
+    if request.path.startswith('/static') or request.path.startswith('/media') or request.path.startswith('/dashboard'):
         return 0
     s_key = request.session.session_key
     ip = get_client_ip(request)
